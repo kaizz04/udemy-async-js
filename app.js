e72370f67720c8b33e607bea60077d493e1e5d16
kaizz04 <kaveesh@marketpushapps.com>
2024-09-14 13:52:33 +0530

    Blocking code & the event loop

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ function trackUserHandler() {
 }
 
 button.addEventListener('click', trackUserHandler);
+
+let result = 0;
+
+setTimeout(()=>{console.log('time out complete')},2000);
+
+
+for(let i=0;i< 100000000;i++){
+  result+=i;
+
+}
+console.log(result);
